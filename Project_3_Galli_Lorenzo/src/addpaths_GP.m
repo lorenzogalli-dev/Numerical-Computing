60bc30ec8638b83df6d148f8f3222d25b0375f84
Lorenzo Galli <lorenzo.galli155@gmail.com>
2025-11-04 23:06:47 +0100

    Delete Project_3_Galli_Lorenzo directory

--- Project_3_Galli_Lorenzo/src/addpaths_GP.m
+++
@@ -1,7 +0,0 @@
-% add necessary paths for the Partitioning Toolbox
-
-addpath ../datasets/
-addpath ../datasets/Mesh_generation/
-addpath ../datasets/2d_3d_meshes
-addpath ../external/
-addpath Visualization/
